3f97656d0670b991b57c06301e7be5d52bfab65e
Miklos Danka <danka.miklos@gmail.com>
2016-11-20 19:35:41 +0100

    Added a new favicon

--- conf.py
+++ conf.py
@@ -132,7 +132,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "resources/lego-designer.ico"
+html_favicon = "resources/techtabor-logo.ico"
 # Attribution:
 # Icon Author: Deleket (Jo)
 # HomePage: http://deleket.deviantart.com/
